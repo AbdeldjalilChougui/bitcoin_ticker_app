d47bd988c6493cce0eccd8ec89ee1613d3f83693
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-03-28 00:15:38 +0100

    push crypto card widget

---
+++ lib/widgets/crypto_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CryptoCard extends StatelessWidget {
+  const CryptoCard({
+    @required this.crypto,
+    @required this.selectedCurrency,
+    @required this.value,
+  });
+
+  final String crypto;
+  final String selectedCurrency;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.lightBlueAccent,
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+        child: Text(
+          '1 $crypto = $value $selectedCurrency',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
