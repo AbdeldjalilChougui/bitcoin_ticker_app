6f35495fe653074ac5b690faa1e60108948e2707
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-03-28 00:15:15 +0100

    commit price screen refactor

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:bitcoin_ticker/models/coin_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:io' show Platform;
@@ -114,40 +115,6 @@ class _PriceScreenState extends State<PriceScreen> {
             child: Platform.isAndroid ? androidDropDown() : iOSPicker(),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class CryptoCard extends StatelessWidget {
-  const CryptoCard({
-    @required this.crypto,
-    @required this.selectedCurrency,
-    @required this.value,
-  });
-
-  final String crypto;
-  final String selectedCurrency;
-  final String value;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Colors.lightBlueAccent,
-      elevation: 5.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-        child: Text(
-          '1 $crypto = $value $selectedCurrency',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.white,
-          ),
-        ),
       ),
     );
   }
